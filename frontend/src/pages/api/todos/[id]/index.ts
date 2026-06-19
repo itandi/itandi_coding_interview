0a7e5ba369dca1f3bd691c1be14a279c69753481
mimorisuzuko <hrky0206@gmail.com>
2026-06-19 14:37:45 +0900

    Fix README

--- frontend/src/pages/api/todos/[id]/index.ts
+++ frontend/src/pages/api/todos/[id]/index.ts
@@ -1,12 +1,21 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import type { TODO, UpdateTODO } from "../../../../types";
+import type { UpdateTODO } from "../../../../types";
 import { db } from "../../../../utils";
 
 export default async function handler(
 	req: NextApiRequest,
-	res: NextApiResponse<TODO[]>
+	res: NextApiResponse
 ) {
-	if (req.method === "PATCH") {
+	if (req.method === "GET") {
+		const todos = await db.get();
+		const index = todos.findIndex(({ id }) => id === req.query.id);
+
+		if (index === -1) {
+			res.status(404).end();
+		} else {
+			res.status(200).json(todos[index]);
+		}
+	} else if (req.method === "PATCH") {
 		const todos = await db.get();
 		const index = todos.findIndex(({ id }) => id === req.query.id);
 
