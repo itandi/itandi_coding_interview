aacb08c8379c99a7aade77df29fb540dd48653ba
mimorisuzuko <hrky0206@gmail.com>
2026-06-19 14:18:12 +0900

    Create API

---
+++ frontend/src/pages/api/todos/[id]/index.ts
@@ -0,0 +1,43 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import type { TODO, UpdateTODO } from "../../../../types";
+import { db } from "../../../../utils";
+
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse<TODO[]>
+) {
+	if (req.method === "PATCH") {
+		const todos = await db.get();
+		const index = todos.findIndex(({ id }) => id === req.query.id);
+
+		if (index === -1) {
+			res.status(404).end();
+		} else {
+			const { completed, content, title } = req.body as UpdateTODO;
+			const todo = todos[index];
+
+			const nextTodos = todos.with(index, {
+				...todos[index],
+				completed: completed ?? todo.completed,
+				content: content ?? todo.content,
+				title: title ?? todo.title,
+				updatedAt: new Date().toISOString()
+			});
+
+			res.status(200).json(await db.save(nextTodos));
+		}
+	} else if (req.method === "DELETE") {
+		const todos = await db.get();
+		const index = todos.findIndex(({ id }) => id === req.query.id);
+
+		if (index === -1) {
+			res.status(404).end();
+		} else {
+			await db.save(todos.toSpliced(index, 1));
+
+			res.status(204).end();
+		}
+	} else {
+		res.status(400).end();
+	}
+}
---
+++ frontend/src/pages/api/todos/index.ts
@@ -0,0 +1,27 @@
+import crypto from "node:crypto";
+import type { NextApiRequest, NextApiResponse } from "next";
+import type { NewTODO, TODO } from "../../../types";
+import { db } from "../../../utils";
+
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse<TODO[]>
+) {
+	if (req.method === "GET") {
+		res.status(200).json(await db.get());
+	} else if (req.method === "POST") {
+		const { content, title } = req.body as NewTODO;
+		const nextTodos = (await db.get()).concat({
+			completed: false,
+			content,
+			createdAt: new Date().toISOString(),
+			id: crypto.randomUUID(),
+			title,
+			updatedAt: new Date().toISOString()
+		});
+
+		res.status(201).json(await db.save(nextTodos));
+	} else {
+		res.status(400).end();
+	}
+}
