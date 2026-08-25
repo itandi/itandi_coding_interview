4ff8fc15b1559d68d8aa0d504b5c978d74a17dac
Bibian Murakawa <h_usuba@ga-tech.co.jp>
2026-08-25 14:43:44 +0900

    [itandi_coding_interview/frontend] パッケージアップデート (#24)
    * Up
    
    * Add useTypeScriptCli
    
    * Use TypeScript 7
    
    * biome check --write
    
    * Up
    
    * Fix
    
    ---------
    
    Co-authored-by: mimorisuzuko <hrky0206@gmail.com>

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+	experimental: {
+		useTypeScriptCli: false
+	},
 	pageExtensions: ["page.tsx", "page.ts"],
 	reactStrictMode: true
 };
